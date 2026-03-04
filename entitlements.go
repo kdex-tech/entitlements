a9bb1a12c622ec7c50d0f3728a9d53b3fab4fa55
Raymond Augé <rayauge@kdex.dev>
2026-03-04 10:31:03 -0500

    eliminate deep cloning
    Signed-off-by: Raymond Augé <rayauge@kdex.dev>

--- entitlements.go
+++ entitlements.go
@@ -2,7 +2,6 @@ package entitlements
 
 import (
 	"fmt"
-	"slices"
 	"strings"
 
 	"github.com/go-logr/logr"
@@ -66,30 +65,31 @@ func (ec *EntitlementsChecker) CalculateResourceRequirements(
 		return nil, fmt.Errorf("resource and resourceName must not be empty")
 	}
 
-	// Make sure never to write back
-	requirements = deepCloneRequirements(requirements)
-
 	// In order for pattern matching to work we need to create and add an identity requirement.
 	identity := fmt.Sprintf("%s:%s:read", resource, resourceName)
 
-	// The identity requirement is added to all requirements
+	// We must return a new structure to avoid modifying the input, but we can do it efficiently.
+	newRequirements := make(Requirements, 0, len(requirements)+1)
+
 	if len(requirements) == 0 {
-		requirements = append(requirements, map[string][]string{
+		newRequirements = append(newRequirements, map[string][]string{
 			ec.defaultScheme: {identity},
 		})
 	} else {
 		for _, req := range requirements {
-			req[ec.defaultScheme] = append(req[ec.defaultScheme], identity)
+			newReq := make(map[string][]string, len(req))
+			for k, v := range req {
+				newReq[k] = v
+			}
+			newReq[ec.defaultScheme] = append(newReq[ec.defaultScheme], identity)
+			newRequirements = append(newRequirements, newReq)
 		}
 	}
 
-	return requirements, nil
+	return newRequirements, nil
 }
 
 // VerifyResourceEntitlements checks if the user's entitlements satisfy the security requirements for a resource instance.
-// Requirements is an array of map[string][]string where each map holds the requirements for a given scheme.
-// The outer array is OR'd - user needs to satisfy at least one scheme.
-// The inner map is AND'd - user needs to satisfy all entitlements in the scheme.
 func (ec *EntitlementsChecker) VerifyResourceEntitlements(
 	resource string,
 	resourceName string,
@@ -100,38 +100,21 @@ func (ec *EntitlementsChecker) VerifyResourceEntitlements(
 		return false, fmt.Errorf("resource and resourceName must not be empty")
 	}
 
-	// Make sure never to write back
-	requirements = deepCloneRequirements(requirements)
-
-	// In order for pattern matching to work we need to create and add an identity requirement.
+	// 1. Check if user satisfies the resource identity requirement.
+	// This is AND'ed into all requirements, so if this fails, everything fails.
 	identity := fmt.Sprintf("%s:%s:read", resource, resourceName)
 
-	// The identity requirement is added to all requirements
-	if len(requirements) == 0 {
-		requirements = append(requirements, map[string][]string{
-			ec.defaultScheme: {identity},
-		})
-	} else {
-		for _, req := range requirements {
-			req[ec.defaultScheme] = append(req[ec.defaultScheme], identity)
-		}
+	// If grantReadyByDefault is true, we implicitly grant the identity entitlement.
+	hasIdentity := ec.grantReadyByDefault || ec.hasEntitlement(entitlements, ec.defaultScheme, identity)
+	if !hasIdentity {
+		return false, nil
 	}
 
-	// Make sure never to write back
-	entitlements = deepCloneEntitlements(entitlements)
-
-	// The identity entitlement is added to all entitlements
-	if ec.grantReadyByDefault {
-		entitlements[ec.defaultScheme] = append(entitlements[ec.defaultScheme], identity)
-	}
-
+	// 2. Verify the rest of the requirements.
 	return ec.VerifyEntitlements(entitlements, requirements), nil
 }
 
 // VerifyEntitlements checks if the user's entitlements satisfy the security requirements.
-// Requirements is an array of map[string][]string where each map is a requirement.
-// The outer array is OR'd - user needs to satisfy at least one requirement.
-// The inner map is AND'd - user needs to satisfy all entitlements in the requirement.
 func (ec *EntitlementsChecker) VerifyEntitlements(
 	entitlements Entitlements,
 	requirements Requirements,
@@ -139,27 +122,6 @@ func (ec *EntitlementsChecker) VerifyEntitlements(
 	// If there are no requirements, access is granted
 	if len(requirements) == 0 {
 		return true
-	}
-
-	// The entitlements granted to anonymous are added to the default scheme
-	if len(ec.anonymousEntitlements) > 0 {
-		// Make sure never to write back
-		entitlements = deepCloneEntitlements(entitlements)
-
-		added := false
-		for scheme, entitlementList := range entitlements {
-			for _, anonEntitlement := range ec.anonymousEntitlements {
-				if scheme == ec.defaultScheme && !slices.Contains(entitlementList, anonEntitlement) {
-					entitlementList = append(entitlementList, anonEntitlement)
-					entitlements[scheme] = entitlementList
-					added = true
-				}
-			}
-		}
-		// When there are no entitlements, the anonymous entitlements are added
-		if !added {
-			entitlements[ec.defaultScheme] = append(entitlements[ec.defaultScheme], ec.anonymousEntitlements...)
-		}
 	}
 
 	defer func() {
@@ -186,13 +148,15 @@ func (ec *EntitlementsChecker) WithLogger(log logr.Logger) *EntitlementsChecker 
 }
 
 func (ec *EntitlementsChecker) satisfiesAndRequirements(entitlements map[string][]string, requirement map[string][]string) bool {
-	// Here requirements are AND'ed - user must have match all
-	for re, requirementList := range requirement {
-		entitlementList, ok := entitlements[re]
-		if !ok {
+	// Here requirements are AND'ed - user must match all
+	for scheme, requirementList := range requirement {
+		// A scheme is satisfied if it's present in entitlements OR it's the default scheme and we have anonymous entitlements.
+		_, ok := entitlements[scheme]
+		if !ok && !(scheme == ec.defaultScheme && len(ec.anonymousEntitlements) > 0) {
 			return false
 		}
-		if !ec.satisfiesRequirement(entitlementList, requirementList) {
+
+		if !ec.satisfiesRequirement(entitlements, scheme, requirementList) {
 			return false
 		}
 	}
@@ -202,9 +166,9 @@ func (ec *EntitlementsChecker) satisfiesAndRequirements(entitlements map[string]
 
 // satisfiesRequirement checks if user entitlements satisfy a single security requirement.
 // Within a requirement, all entitlements must be present (AND logic).
-func (ec *EntitlementsChecker) satisfiesRequirement(entitlements []string, requirement []string) bool {
+func (ec *EntitlementsChecker) satisfiesRequirement(entitlements map[string][]string, scheme string, requirement []string) bool {
 	for _, curRequirement := range requirement {
-		if !ec.hasEntitlement(entitlements, curRequirement) {
+		if !ec.hasEntitlement(entitlements, scheme, curRequirement) {
 			return false
 		}
 	}
@@ -213,12 +177,25 @@ func (ec *EntitlementsChecker) satisfiesRequirement(entitlements []string, requi
 }
 
 // hasEntitlement checks if the user has a specific entitlement.
-func (ec *EntitlementsChecker) hasEntitlement(entitlements []string, requirement string) bool {
-	for _, entitlement := range entitlements {
-		if ec.entitlementMatches(entitlement, requirement) {
-			return true
+func (ec *EntitlementsChecker) hasEntitlement(entitlements map[string][]string, scheme string, requirement string) bool {
+	// Check user-provided entitlements for this scheme
+	if entitlementList, ok := entitlements[scheme]; ok {
+		for _, entitlement := range entitlementList {
+			if ec.entitlementMatches(entitlement, requirement) {
+				return true
+			}
 		}
 	}
+
+	// Check anonymous entitlements if we are checking the default scheme
+	if scheme == ec.defaultScheme {
+		for _, entitlement := range ec.anonymousEntitlements {
+			if ec.entitlementMatches(entitlement, requirement) {
+				return true
+			}
+		}
+	}
+
 	return false
 }
 
@@ -275,57 +252,3 @@ func (ec *EntitlementsChecker) entitlementMatches(entitlement, requirement strin
 	return parts[1] == requiredParts[1]
 }
 
-func deepCloneEntitlements(entitlements map[string][]string) map[string][]string {
-	if entitlements == nil {
-		return nil
-	}
-
-	// 1. Clone the outer slice
-	clone := make(map[string][]string, len(entitlements))
-
-	for key, entitlementList := range entitlements {
-		if entitlementList == nil {
-			clone[key] = nil
-			continue
-		}
-
-		// 2. Clone the inner Slice
-		newEntitlementList := make([]string, len(entitlementList))
-		copy(newEntitlementList, entitlementList)
-		clone[key] = newEntitlementList
-	}
-
-	return clone
-}
-
-func deepCloneRequirements(requirements Requirements) Requirements {
-	if requirements == nil {
-		return nil
-	}
-
-	// 1. Clone the outer slice
-	clone := make(Requirements, len(requirements))
-
-	for i, reqMap := range requirements {
-		if reqMap == nil {
-			continue
-		}
-
-		// 2. Clone the Map
-		newMap := make(map[string][]string, len(reqMap))
-		for key, requirementList := range reqMap {
-			if requirementList == nil {
-				newMap[key] = nil
-				continue
-			}
-
-			// 3. Clone the inner Slice
-			newRequirementList := make([]string, len(requirementList))
-			copy(newRequirementList, requirementList)
-			newMap[key] = newRequirementList
-		}
-		clone[i] = newMap
-	}
-
-	return clone
-}
